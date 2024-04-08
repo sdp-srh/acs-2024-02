13dba84d7e2ad7f1d341930072022417a84c8435
sdp-srh <105343612+sdp-srh@users.noreply.github.com>
2024-04-08 16:35:47 +0200

    express server

--- example-node/index.js
+++ example-node/index.js
@@ -1,12 +1,8 @@
-
 const express = require('express')
 const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello ACS course!')
+app.get('/', function (req, res) {
+  res.send('<h1>Hello ACS Course</h1><p>My first Express app</p>')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(3000)
--- example-node/test.js
+++ example-node/test.js
@@ -1,1 +1,7 @@
-console.log('node is running...')
+const add = (x,y) => {
+    return x + y
+}
+
+
+console.log('Hello ACS Course')
+console.log(add(2,3))

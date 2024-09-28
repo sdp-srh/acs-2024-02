6236e6bbce7ff01b40cc60c36124e229e16c72e7
sdp-srh <105343612+sdp-srh@users.noreply.github.com>
2024-09-28 11:48:47 +0200

    admin users page

--- soccermobile/src/views/AdminUsersPage.vue
+++ soccermobile/src/views/AdminUsersPage.vue
@@ -8,34 +8,43 @@
     <ion-content :fullscreen="true">
       <ion-card>
         <ion-text>
-          <h3>
-            Erstelle einen neuen Benutzer
-          </h3>
+          <h3>Spieler anlegen oder ändern</h3>
         </ion-text>
       </ion-card>
       <ion-card>
         <ion-list>
           <ion-item>
+            <ion-label position="floating">Spieler</ion-label>
+            <ion-select :value="selectedPlayerId" @ionChange="handlePlayerChange" interface="popover">
+              <ion-select-option v-for="player in players" :key="player.id" :value="player.id">
+                {{ player.name }}
+              </ion-select-option>
+            </ion-select>
+          </ion-item>
+          <ion-item>
             <ion-label position="floating">Nickname</ion-label>
-            <ion-input v-model="userid" type="text"></ion-input>
+            <ion-input :value="nickname" type="text" @ionInput="nickname = $event.target.value"></ion-input>
           </ion-item>
           <ion-item>
             <ion-label position="floating">Passwort</ion-label>
-            <ion-input v-model="password" type="text"></ion-input>
+            <ion-input :value="password" type="text" @ionInput="password = $event.target.value"></ion-input>
           </ion-item>
           <ion-item>
             <ion-label position="floating">Name</ion-label>
-            <ion-input v-model="fullname" type="text"></ion-input>
+            <ion-input :value="fullname" type="text" @ionInput="fullname = $event.target.value"></ion-input>
           </ion-item>
           <ion-item>
             <ion-label position="floating">Email</ion-label>
-            <ion-input v-model="email" type="text"></ion-input>
+            <ion-input :value="mail" type="text" @ionInput="mail = $event.target.value"></ion-input>
           </ion-item>
           <ion-item>
             <ion-label position="floating">Secret</ion-label>
-            <ion-input v-model="secret" type="password"></ion-input>
+            <ion-input :value="scode" type="text"  @ionInput="scode = $event.target.value" ></ion-input>
           </ion-item>
-          <!-- save and cancel button -->
+          <ion-item>
+            <h3>{{ scode  }}</h3>
+          </ion-item>
+          <!-- Save and reset buttons -->
           <ion-item>
             <ion-button @click="save">Speichern</ion-button>
             <ion-button @click="reset">Reset</ion-button>
@@ -47,52 +56,108 @@
 </template>
 
 <script setup>
-import { IonItem, IonLabel, IonInput, IonList, IonReorder, IonReorderGroup, IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonCard, IonText } from '@ionic/vue'
-import { onMounted, ref } from 'vue'
-import { toastController } from '@ionic/vue';
+import { ref, onMounted } from 'vue'
+import { toastController } from '@ionic/vue'
 
+const nickname = ref('') // Nickname field
+const password = ref('') // Password field
+const fullname = ref('') // Full name field
+const mail = ref('') // Email field
+const scode = ref('') // Secret field (not bound to the form, for security reasons)
 
-const user = ref({ userid: '', password: '', fullname: '', email: '', secret: '' })
+const players = ref([]) // Players array
+const selectedPlayerId = ref('NEW') // Default to 'NEW' (Neuer Spieler)
+
+// Fetch players data when the component is mounted
 onMounted(async () => {
+  await loadPlayers()
 })
 
+// Load players and add "Neuer Spieler" to the list
+const loadPlayers = async () => {
+  const response = await fetch('/api/player')
+  const data = await response.json()
+  const newPlayer = { id: 'NEW', name: 'Neuer Spieler', nickname: '' }
+  players.value = [newPlayer, ...data]
+}
+
+// Handle player selection change
+const handlePlayerChange = (event) => {
+  selectedPlayerId.value = event.detail.value
+  updateFormWithSelectedPlayer(selectedPlayerId.value)
+}
+
+// Update form based on selected player
+const updateFormWithSelectedPlayer = (newId) => {
+  if (newId === 'NEW') {
+    // Reset form for a new player
+    resetForm()
+  } else {
+    // Load existing player data into the form
+    const selectedPlayer = players.value.find(player => player.id === newId)
+    if (selectedPlayer) {
+      // Directly update the properties of the `user` object to ensure reactivity
+
+      nickname.value = selectedPlayer.nickname || ''
+      password.value = selectedPlayer.password || '' // Default empty if not provided
+      fullname.value = selectedPlayer.fullname || ''
+      mail.value = selectedPlayer.mail || ''
+    }
+  }
+}
+
+// Save user data (you can modify this to fit your API)
 const save = async () => {
-  console.log('saving new user')
+  
+  console.log('secret:', scode.value)
+  const playerObJ = {
+    id: selectedPlayerId.value,
+    nickname: nickname.value,
+    password: password.value,
+    fullname: fullname.value,
+    mail: mail.value,
+    scode: scode.value
+  }
+  
   const response = await fetch('/api/admin/user', {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      'x-admin-secret': scode.value
     },
     body: JSON.stringify(user.value)
   })
   const message = await response.json()
   showMessage(message.msg)
+  
 }
 
-const reset = async () => {
-    user.value = { userid: '', password: '', fullname: '', email: '', secret: '' }
-  }
+// Reset form to blank fields and reset selected player to 'NEW'
+const reset = () => {
+  selectedPlayerId.value = 'NEW'
+  resetForm()
+}
 
+// Reset form fields
+const resetForm = () => {
+  nickname.value = ''
+  password.value = ''
+  fullname.value = ''
+  mail.value = ''
+}
+
+// Show a message using Ionic's toast
 const showMessage = async (message, color = "light", duration = 2000) => {
-  const toast = await toastController
-    .create({
-      message: message,
-      duration: duration,
-      color: color,
-      position: 'bottom',
-    });
-  toast.present();
+  const toast = await toastController.create({
+    message: message,
+    duration: duration,
+    color: color,
+    position: 'bottom'
+  })
+  toast.present()
 }
-
 </script>
 
 <style scoped>
-.team-icon {
-  height: 1.2em;
-  /* Adjust this value as needed */
-  width: auto;
-  /* Keeps the aspect ratio intact */
-  vertical-align: middle;
-  /* Aligns the image vertically with the text */
-}
+/* Optional: Add any specific styles here */
 </style>
